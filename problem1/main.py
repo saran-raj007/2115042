38fc402e09ce1c1597613a16cbf4f1b37daea11a
saran-raj007 <k.saranraj000@gmail.com>
2024-06-20 12:29:35 +0530

    problem1

--- problem1/main.py
+++ problem1/main.py
@@ -1,19 +1,18 @@
 from fastapi import FastAPI
 import requests
 
-evenurl="http://20.244.56.144/test/even"
-randurl="http://20.244.56.144/test/rand"
-primeurl="http://20.244.56.144/test/primes"
-fiburl="http://20.244.56.144/test/fibo"
+# evenurl="http://20.244.56.144/test/even"
+# randurl="http://20.244.56.144/test/rand"
+# primeurl="http://20.244.56.144/test/primes"
+# fiburl="http://20.244.56.144/test/fibo"
 
 
 
 
-headers = {
-    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzE4ODYwMTI1LCJpYXQiOjE3MTg4NTk4MjUsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6IjY2YWQ2Y2Q1LTJjYmMtNGFiOS05MDc4LTQzOThlY2NiMGMyOSIsInN1YiI6IjIxMTUwNDJAbmVjLmVkdS5pbiJ9LCJjb21wYW55TmFtZSI6IlNSIGNvbXBhbnkiLCJjbGllbnRJRCI6IjY2YWQ2Y2Q1LTJjYmMtNGFiOS05MDc4LTQzOThlY2NiMGMyOSIsImNsaWVudFNlY3JldCI6Ikp3bFlET1dZa1h4ZkppTVoiLCJvd25lck5hbWUiOiJTYXJhbnJhaiBLIiwib3duZXJFbWFpbCI6IjIxMTUwNDJAbmVjLmVkdS5pbiIsInJvbGxObyI6IjIxMTUwNDIifQ.DSGGB_Z2stGo7WmeXnFaLXti3U6nSBHJDLhZyN_HFlU',
-    'Content-Type': 'application/json',
-    'Accept': 'application/json'
-}
+# headers = {
+#     'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzE4ODY2NDM4LCJpYXQiOjE3MTg4NjYxMzgsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6IjY2YWQ2Y2Q1LTJjYmMtNGFiOS05MDc4LTQzOThlY2NiMGMyOSIsInN1YiI6IjIxMTUwNDJAbmVjLmVkdS5pbiJ9LCJjb21wYW55TmFtZSI6IlNSIGNvbXBhbnkiLCJjbGllbnRJRCI6IjY2YWQ2Y2Q1LTJjYmMtNGFiOS05MDc4LTQzOThlY2NiMGMyOSIsImNsaWVudFNlY3JldCI6Ikp3bFlET1dZa1h4ZkppTVoiLCJvd25lck5hbWUiOiJTYXJhbnJhaiBLIiwib3duZXJFbWFpbCI6IjIxMTUwNDJAbmVjLmVkdS5pbiIsInJvbGxObyI6IjIxMTUwNDIifQ.UNyUeLJL3yvaXJZrfN9L-7wdDZ68HsIWnv9A88olI7w',
+    
+# }
 
 
 app=FastAPI()
@@ -23,11 +22,15 @@ window=[]
 
 @app.get("/number/e")
 def avgnum():
-    response=requests.get(evenurl,headers=headers)
-    print(response)
+   
     
-    server_res=response.numbers
-    prev_window=window
+   
+    server_res=[26,
+        28,
+        30,
+        32,
+        34]
+    prev_window=window.copy()
     for i in server_res:
         if len(window)==10:
             window.pop(0)
@@ -44,11 +47,15 @@ def avgnum():
 
 @app.get("/number/p")
 def avgnum():
-    response=requests.get(primeurl,headers=headers)
-    print(response)
     
-    server_res=response.numbers
-    prev_window=window
+    
+    
+    server_res=[47,
+        53,
+        59,
+        61,
+        67]
+    prev_window=window.copy()
     for i in server_res:
         if len(window)==10:
             window.pop(0)
@@ -64,11 +71,20 @@ def avgnum():
 
 @app.get("/number/r")
 def avgnum():
-    response=requests.get(randurl,headers=headers)
-    print(response)
     
-    server_res=response.numbers
-    prev_window=window
+    
+    server_res=[22,
+        20,
+        9,
+        6,
+        15,
+        12,
+        2,
+        19,
+        25,
+        7,
+        4]
+    prev_window=window.copy()
     for i in server_res:
         if len(window)==10:
             window.pop(0)
@@ -84,11 +100,19 @@ def avgnum():
 
 @app.get("/number/f")
 def avgnum():
-    response=requests.get(fiburl,headers=headers)
-    print(response)
     
-    server_res=response.numbers
-    prev_window=window
+    server_res=[377,
+        610,
+        987,
+        1597,
+        2584,
+        4181,
+        6765,
+        10946,
+        17711,
+        28657,
+        46368]
+    prev_window=window.copy()
     for i in server_res:
         if len(window)==10:
             window.pop(0)
